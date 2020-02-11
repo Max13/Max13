06820fb7027dd2bd2ad1a9980f67e76fe706c388
Adnan RIHAN <adnan@rihan.fr>
2020-02-11 02:39:23 +0100

    Set correct lang after doctype

--- source/index.blade.php
+++ source/index.blade.php
@@ -1,8 +1,8 @@
 <!DOCTYPE html>
-<!--[if lt IE 7]> <html class="no-js ie6 oldie" lang="en"> <![endif]-->
-<!--[if IE 7]>    <html class="no-js ie7 oldie" lang="en"> <![endif]-->
-<!--[if IE 8]>    <html class="no-js ie8 oldie" lang="en"> <![endif]-->
-<!--[if IE 9]>    <html class="no-js ie9" lang="en"> <![endif]-->
+<!--[if lt IE 7]> <html class="no-js ie6 oldie" lang="fr"> <![endif]-->
+<!--[if IE 7]>    <html class="no-js ie7 oldie" lang="fr"> <![endif]-->
+<!--[if IE 8]>    <html class="no-js ie8 oldie" lang="fr"> <![endif]-->
+<!--[if IE 9]>    <html class="no-js ie9" lang="fr"> <![endif]-->
 <!--[if gt IE 9]><!-->
 <html><!--<![endif]-->
 <head>
